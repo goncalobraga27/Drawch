fb9cd72ea989d688315ffe02fb898cba8c1dcb1c
goncalobraga27 <a97541@alunos.uminho.pt>
2024-04-20 18:55:29 +0100

    WebApp

--- ShiftAppens/ShiftAppensApp/urls.py
+++ ShiftAppens/ShiftAppensApp/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path("contacts",views.contacts,name="contacts"),
     path("paint",views.paint,name="paint"),
     path("paint.js",views.get_paint_js,name="get_paint.js"),
-    path("paint.css",views.get_paint_css,name="get_paint.css")
+    path("paint.css",views.get_paint_css,name="get_paint.css"), 
+    path("background.jpeg",views.get_background,name="get_background"),
+    path("favicon.png",views.get_favicon,name="get_favicon"),
+    path("home.png",views.get_home,name="get_home"),
+    path("website.png",views.get_website,name="get_website"),
+    path("about.png",views.get_about,name="get_about")
 ]
--- ShiftAppens/ShiftAppensApp/views.py
+++ ShiftAppens/ShiftAppensApp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-
+import os
+from django.http import FileResponse, HttpResponseNotFound
 # Create your views here.
 def home(request):
     return render(request,"home.html")
@@ -19,3 +20,38 @@ def get_paint_js(request):
 
 def get_paint_css(request):
     return render(request,"paint.css")
+
+def get_background(request):
+    background_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "background.jpeg")
+    if os.path.exists(background_path):
+        return FileResponse(open(background_path, 'rb'), content_type='image/jpeg')
+    else:
+        return HttpResponseNotFound()
+    
+def get_favicon(request):
+    background_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "favicon.png")
+    if os.path.exists(background_path):
+        return FileResponse(open(background_path, 'rb'), content_type='image/png')
+    else:
+        return HttpResponseNotFound()
+    
+def get_home(request):
+    background_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "home.png")
+    if os.path.exists(background_path):
+        return FileResponse(open(background_path, 'rb'), content_type='image/png')
+    else:
+        return HttpResponseNotFound()
+
+def get_website(request):
+    background_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "website.png")
+    if os.path.exists(background_path):
+        return FileResponse(open(background_path, 'rb'), content_type='image/png')
+    else:
+        return HttpResponseNotFound()
+
+def get_about(request):
+    background_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "about.png")
+    if os.path.exists(background_path):
+        return FileResponse(open(background_path, 'rb'), content_type='image/png')
+    else:
+        return HttpResponseNotFound()
